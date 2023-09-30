ad2c3b9205aa02753609cfc926a0f19925109331
BHUPESH BAJPAI <bhupeshbajpai@BHUPESHs-MacBook-Air.local>
2023-09-30 19:01:42 +0530

    structure change

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const app = express();
 const cors = require("cors")
 
-const adminRouter = require('../backend/routes/admin');
-const userRouter = require('../backend/routes/user');
+const adminRouter = require('../routes/admin');
+const userRouter = require('../routes/user');
 
 app.use(express.json());
 app.use(cors())
