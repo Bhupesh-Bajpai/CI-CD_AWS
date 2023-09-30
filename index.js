cc749899d0338bd25cd6d7fc7d78e0ea81bcf1f3
BHUPESH BAJPAI <bhupeshbajpai@BHUPESHs-MacBook-Air.local>
2023-09-30 13:33:12 +0530

    new backend

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const cors = require("cors")
+
+const adminRouter = require('../backend/routes/admin');
+const userRouter = require('../backend/routes/user');
+
+app.use(express.json());
+app.use(cors())
+
+// connect with user routes
+app.use("/admin",adminRouter);
+app.use('/users',userRouter)
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://bajpaibhupesh25:Bhupesh2502@cluster0.rrzvaih.mongodb.net/courses', { useNewUrlParser: true, useUnifiedTopology: true, dbName: "courses" });
+
+
+
+
+
+
+app.listen(3000, () => console.log('Server running on port 3000'));
